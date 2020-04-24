bd9c3e76390fbb03168f3f7117332bb4b6d0c87d
Ramon <juanramoncarceles@gmail.com>
2020-04-24 19:12:35 +0200

    Code simplified

--- index.js
+++ index.js
@@ -6,105 +6,90 @@
  * Website language codes used in the url.
  * Set the language codes used as the values of the entries.
  */
-const langCodes = {
-  es: "es",
-  fr: "fr",
-  it: "it",
-  de: "de",
-  zh: "zh-hans",
-  ko: "ko",
-  en: "en"
-}
 
-
-// Regular expressions according to the Official Language Codes for the browsers.
-// Create only the ones available in the website.
-const browserLangRegExps = {
-  es: new RegExp(/^es-{0,1}/),
-  fr: new RegExp(/^fr-{0,1}/),
-  it: new RegExp(/^it-{0,1}/),
-  de: new RegExp(/^de-{0,1}/),
-  zh: new RegExp(/^zh-{0,1}/),
-  ko: new RegExp(/^ko-{0,1}/),
-  en: new RegExp(/^en-{0,1}/)
-}
-
-/**
- * Creates an object with all the regular expressions to check the web site url language code.
- * @param {Object} languageCodes The object with the website language codes.
- */
-function createUrlLangRegExpsObj(languageCodes) {
-  const regExpsObj = {};
-  for (let key in languageCodes) {
-    const regExpValue = '^\/' + languageCodes[key] + '\/';
-    regExpsObj[key] = new RegExp(regExpValue);
+// IMPORTATN LEAVE EMPTY string THE urlCode value OF THE DEFAULT LANG
+// put the beginning of the browser code, until the dash. should be the official
+// Add the available languages in the website.
+const langCodes2 = [
+  {
+    urlCode: 'es',
+    browserCode: 'es-',
+    sentence: 'Ver en Español'
+  },
+  {
+    urlCode: 'fr',
+    browserCode: 'fr-',
+    sentence: 'Voir en Français'
+  },
+  {
+    urlCode: 'it',
+    browserCode: 'it-',
+    sentence: 'Vedere in Italiano'
+  },
+  {
+    urlCode: 'de',
+    browserCode: 'de-',
+    sentence: 'Ansicht auf Deutsch'
+  },
+  {
+    urlCode: 'zh-hans',
+    browserCode: 'zh-',
+    sentence: '用中文查看'
+  },
+  {
+    urlCode: 'ko',
+    browserCode: 'ko-',
+    sentence: '한국어로보기'
+  },
+  {
+    urlCode: '',
+    browserCode: 'en-',
+    sentence: 'View in English'
   }
-  return regExpsObj;
-}
+];
 
-const urlLangRegExps = createUrlLangRegExpsObj(langCodes);
 
-// According to the available languages of the web site.
-// Last one should be the default language, which corresponds to the url without lang code.
-function getUrlLang() {
+// Returns the index of the code lang in langCodes2.
+function getUrlLangIndex() {
   const websitePath = window.location.pathname;
-  if (urlLangRegExps.es.test(websitePath)) {
-    return langCodes.es;
-  } else if (urlLangRegExps.fr.test(websitePath)) {
-    return langCodes.fr;
-  } else if (urlLangRegExps.it.test(websitePath)) {
-    return langCodes.it;
-  } else if (urlLangRegExps.de.test(websitePath)) {
-    return langCodes.de;
-  } else if (urlLangRegExps.zh.test(websitePath)) {
-    return langCodes.zh;
-  } else if (urlLangRegExps.ko.test(websitePath)) {
-    return langCodes.ko;
-  } else {
-    return langCodes.en;
-  }
+  const xx = langCodes2.findIndex(lang => {
+    const langRegExp = new RegExp('^\/' + lang.urlCode + '\/');
+    return langRegExp.test(websitePath);
+  });
+  // If -1 return the index of the empty, which is the default
+  if (xx === -1) 
+    return langCodes2.findIndex(lang => lang.urlCode === '');
+  else
+    return xx;
 }
 
-/**
- * Checks if the browser has a language that the website supports,
- * otherwise it returns undefined.
- */
-function getBrowserLang() {
+// If no code matches it is intended that the website doesnt have the browsers language and -1 is returned.
+function getBrowserLangIndex() {
   const browserLang = navigator.language || navigator.userLanguage;
-  if (browserLangRegExps.es.test(browserLang)) {
-    return langCodes.es;
-  } else if (browserLangRegExps.fr.test(browserLang)) {
-    return langCodes.fr;
-  } else if (browserLangRegExps.it.test(browserLang)) {
-    return langCodes.it;
-  } else if (browserLangRegExps.de.test(browserLang)) {
-    return langCodes.de;
-  } else if (browserLangRegExps.zh.test(browserLang)) {
-    return langCodes.zh;
-  } else if (browserLangRegExps.ko.test(browserLang)) {
-    return langCodes.ko;
-  } else if (browserLangRegExps.en.test(browserLang)) {
-    return langCodes.en;
-  } else {
-    return undefined;
-  }
+  return langCodes2.findIndex(lang => {
+    const langRegExp = new RegExp('^' + lang.browserCode + '{0,1}');
+    return langRegExp.test(browserLang);
+  });
 }
-
 
 // Here is checked if a redirection could be necessary.
 // If the getBrowserLang returns undefined it means that we have no website language that can match, so nothing is done.
 // If there is a supported browser language and it is different than the one on the current website then offer the website with the browser lang.
-if (getBrowserLang() !== undefined && getBrowserLang() !== getUrlLang()) {
-  let newUrl;
-  if (getUrlLang() === langCodes.en) { // If the url had the default lang (english) then only add the browser one at the beginning of the path.
-    newUrl = window.location.origin + "/" + getBrowserLang() + window.location.pathname;
-  } else { // If the url had another language, then replace it by the one of the browser.
-    const urlPathname = window.location.pathname.match(/^\/[a-z-]+(\/.+)/);
-    newUrl = window.location.origin + (getBrowserLang() !== langCodes.en ? "/" + getBrowserLang() : '') + (urlPathname !== null ? urlPathname[1] : '');
+if (getBrowserLangIndex() !== -1 && getBrowserLangIndex() !== getUrlLangIndex()) {
+  const newUrlLangCode = langCodes2[getBrowserLangIndex()].urlCode;
+  // remove the existing url lang if any
+  let urlPathname;
+  if (langCodes2[getUrlLangIndex()].urlCode !== '') {
+    const pathRegExp = new RegExp('^\/' + langCodes2[getUrlLangIndex()].urlCode + '(\/.+)');
+    urlPathnameRes = window.location.pathname.match(pathRegExp);
+    urlPathname = urlPathnameRes !== null ? urlPathnameRes[1] : '/';
+  } else {
+    urlPathname = window.location.pathname;
   }
+  const newUrl = window.location.origin + (newUrlLangCode !== '' ? "/" + newUrlLangCode : '') + urlPathname;
   const changeLangContainer = document.getElementById('changeLangContainer');
   changeLangContainer.style.display = "block"; // TODO add a class that has opacity with transition.
-  changeLangContainer.innerHTML = `<a href="${newUrl}">View the site in ${getBrowserLang()}</a>`;
-} else if (getBrowserLang() === undefined) {
+  changeLangContainer.innerHTML = `<a href="${newUrl}">${langCodes2[getBrowserLangIndex()].sentence}</a>`;
+} else if (getBrowserLangIndex() === -1) {
   console.warn("Sorry but we don't have our website in your browser's language.");
 }
