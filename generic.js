78bf8d07f4ed695a7b477c86893b056a82c64c30
Ramon <juanramoncarceles@gmail.com>
2020-05-14 17:05:40 +0200

    test console.log() removed

--- generic.js
+++ generic.js
@@ -4,7 +4,6 @@ const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY === 0) {
-    console.log('h');
     header.classList.remove('fixed');
   } else {
     header.classList.add('fixed');
